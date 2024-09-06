2620fc3391500d4f7ce08207a92ad119660ca97f
Nancy-Lara-Baca <a.22308051281225@cbtis128.edu.mx>
2024-09-06 09:33:21 -0600

    Add files via upload
    Subí 2 archivos

---
+++ miscadenas.py
@@ -0,0 +1,23 @@
+"""nancy lara
+trabajando cadenas"""
+
+
+nombre="Nancy"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["N","a","n"]
+for chicas in MisNovias:
+    print(chicas)
+    
+
+    print("otro ejemplo")
+    b= "Nancy Lara"
+print(b[2:5])
+a = "nancy lara"
+print(a.upper())
+print("concatenar")
+a = "maria"
+b = "Perez"
+c = a + " " + b
+print(c)
